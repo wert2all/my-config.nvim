743b68c4fd474d23f7b8613d4e3776a631a34076
Serhii Shvets <wert2all@gmail.com>
2024-10-26 13:49:29 +0300

    feat: add configs

--- init.lua
+++ init.lua
@@ -1,3 +1,6 @@
+require 'core.options'
+require 'core.keymaps'
+
 local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
   local lazyrepo = 'https://github.com/folke/lazy.nvim.git'
@@ -9,4 +12,22 @@ end
 vim.opt.rtp:prepend(lazypath)
 
 require('lazy').setup({
+	{
+    "nvim-neo-tree/neo-tree.nvim",
+    branch = "v3.x",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+      "MunifTanjim/nui.nvim",
+      -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
+    }
+},
+	{
+		"EdenEast/nightfox.nvim", 
+		lazy=false,
+		priority=1000
+	}
 })
+
+vim.cmd("colorscheme nightfox")
+
